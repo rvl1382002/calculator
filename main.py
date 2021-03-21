b1f6d88042173205fd61912f35565a86c59aaaa0
Riddhish <44116452+rvl1382002@users.noreply.github.com>
2021-03-21 20:02:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+#This is the main file for our project
+#All the functions will be written in a different file which will be called from this file 'main.py'
+#This will be completely graphical calculator using tkinter
